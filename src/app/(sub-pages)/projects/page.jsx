81e49be213c4e314a46ea9eaecc0f5d82148d545
ismaelp96 <ismael6991@live.com>
2024-03-14 11:55:02 -0300

    Subpages Layout #5

--- src/app/(sub-pages)/projects/page.jsx
+++ src/app/(sub-pages)/projects/page.jsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 
-import bg from '../../../public/background/projects-background.png';
+import bg from '../../../../public/background/projects-background.png';
 import ProjectList from '@/components/projects';
-import { projectsData } from '../data';
+import { projectsData } from '../../data';
 import RenderModel from '@/components/RenderModel';
 import dynamic from 'next/dynamic';
 const Staff = dynamic(() => import('@/components/models/Staff'), {
@@ -11,21 +11,20 @@ const Staff = dynamic(() => import('@/components/models/Staff'), {
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between relative'>
+    <>
       <Image
         priority
         sizes='100vw'
         src={bg}
         alt='background-image'
-        fill
-        className='-z-50 w-full h-full object-cover object-center opacity-50'
+        className='-z-50 w-full h-full object-cover object-center opacity-25 fixed top-0 left-0'
       />
       <ProjectList projects={projectsData} />
-      <div className='flex items-center justify-center fixed top-20 left-0 h-screen'>
+      <div className='flex items-center justify-center fixed top-20 -left-24 h-screen'>
         <RenderModel>
           <Staff />
         </RenderModel>
       </div>
-    </main>
+    </>
   );
 }
