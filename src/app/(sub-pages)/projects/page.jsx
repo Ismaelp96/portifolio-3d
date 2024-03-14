e9d93048fc4d4f84f76d0792181eaa55089f95ff
ismaelp96 <ismael6991@live.com>
2024-03-14 16:04:06 -0300

    About Page #6

--- src/app/(sub-pages)/projects/page.jsx
+++ src/app/(sub-pages)/projects/page.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import dynamic from 'next/dynamic';
 
 import bg from '../../../../public/background/projects-background.png';
 import ProjectList from '@/components/projects';
 import { projectsData } from '../../data';
 import RenderModel from '@/components/RenderModel';
-import dynamic from 'next/dynamic';
+
 const Staff = dynamic(() => import('@/components/models/Staff'), {
   ssr: false,
 });
@@ -17,7 +18,7 @@ export default function Home() {
         sizes='100vw'
         src={bg}
         alt='background-image'
-        className='-z-50 w-full h-full object-cover object-center opacity-25 fixed top-0 left-0'
+        className='-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50'
       />
       <ProjectList projects={projectsData} />
       <div className='flex items-center justify-center fixed top-20 -left-24 h-screen'>
