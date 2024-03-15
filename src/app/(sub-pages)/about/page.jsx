d7a5dff5f5c73f4bd9c8393304cf93dc448d7c84
ismaelp96 <ismael6991@live.com>
2024-03-15 17:17:08 -0300

    About for me - GitHub stats #11

--- src/app/(sub-pages)/about/page.jsx
+++ src/app/(sub-pages)/about/page.jsx
@@ -27,10 +27,10 @@ export default function Home() {
       <div className='relative w-full h-screen flex flex-col items-center justify-center'>
         <div className='absolute flex flex-col items-center text-center top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2'>
           <h1 className='font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent'>
-            CodeBucks
+            Ismael
           </h1>
           <p className='font-light text-foreground text-xl'>
-            Meet the wizard behind this portfolio
+            Conheça o mago por trás deste portfólio
           </p>
         </div>
       </div>
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -20,34 +20,35 @@ const AboutDetails = () => {
       <div className='grid grid-cols-12 gap-8 w-full'>
         <ItemLayout className={'col-span-8 row-span-2 flex-col items-start'}>
           <h2 className='text-2xl text-left w-full capitalize'>
-            Architect of Enchantment
+            Arquiteto do Encantamento
           </h2>
-          <p className='font-light'>
-            My journey in web development is powered by an array of mystical
-            tools and languages, with JavaScript casting the core of my
-            enchantments. I wield frameworks like React.js and Next.js with
-            precision, crafting seamless portals (websites) that connect realms
-            (users) across the digital universe. The ancient arts of the
-            Jamstack empower me to create fast, secure, and dynamic experiences,
-            while my design skills ensure every creation is not only functional
-            but visually captivating. Join me as I continue to explore new
-            spells and technologies to shape the future of the web.
+          <p className='text-lg'>
+            Minha jornada no desenvolvimento web é alimentada por uma série de
+            conhecimentos místicos ferramentas e linguagens, com JavaScript
+            lançando o núcleo do meu encantamentos. Eu uso frameworks como
+            React.js e Next.js com precisão, criando portais(sites) integrados
+            que conectam reinos(usuários) em todo o universo digital. As antigas
+            artes do Jamstack me permite criar experiências rápidas, seguras e
+            dinâmicas, enquanto minhas habilidades de design garantem que cada
+            criação não seja apenas funcional mas visualmente cativante.
+            Junte-se a mim enquanto continuo a explorar novos feitiços e
+            tecnologias para moldar o futuro da web.
           </p>
         </ItemLayout>
         <ItemLayout className={'col-span-4 text-accent'}>
           <p className='font-semibold w-full text-left text-5xl'>
-            25+ <sub className='font-semibold text-base'>projects</sub>
+            25+ <sub className='font-semibold text-base'>Projetos</sub>
           </p>
         </ItemLayout>
         <ItemLayout className={'col-span-4 text-accent'}>
           <p className='font-semibold w-full text-left text-5xl'>
             1+{' '}
-            <sub className='font-semibold text-base'> years of experience</sub>
+            <sub className='font-semibold text-base'> anos de experiência</sub>
           </p>
         </ItemLayout>
         <ItemLayout className={'col-span-4 !p-0'}>
           <img
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=Ismaelp96&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&locale=pt-BR`}
+            src={`https://github-readme-stats.vercel.app/api/top-langs?username=Ismaelp96&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&locale=pt-BR`}
             alt='Ismaelp96 Git Stats'
             loading='lazy'
             className='w-full h-auto'
@@ -55,7 +56,7 @@ const AboutDetails = () => {
         </ItemLayout>
         <ItemLayout className={'col-span-8 !p-0'}>
           <img
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=Ismaelp96&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&locale=pt-BR`}
+            src={`https://github-readme-stats.vercel.app/api?username=Ismaelp96&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&locale=pt-BR`}
             alt='Ismaelp96 Git Stats language'
             loading='lazy'
             className='w-full h-auto'
@@ -63,21 +64,19 @@ const AboutDetails = () => {
         </ItemLayout>
         <ItemLayout className={'col-span-6 !p-0'}>
           <img
-            className='w-full h-auto flex-wrap'
-            src={`https://skillicons.dev/icons?i=js,html,css,react,nextjs,ts,tailwind,sass,postman,github,npm,nodejs`}
-            alt='Skill icons'
+            className='w-full h-auto'
+            src='https://github-readme-streak-stats.herokuapp.com?user=Ismaelp96&theme=dark&hide_border=true&type=png&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B'
+            alt='Ismaelp96 Git Streak'
             loading='lazy'
           />
         </ItemLayout>
         <ItemLayout className={'col-span-6 !p-0'}>
-          <a href='https://git.io/streak-stats'>
-            <img
-              src='https://github-readme-streak-stats.herokuapp.com?user=Ismaelp96&theme=dark&hide_border=true&type=png&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B'
-              alt='Ismaelp96 Git Streak'
-              loading='lazy'
-              className='w-full h-auto'
-            />
-          </a>
+          <img
+            src={`https://github-readme-stats.vercel.app/api/pin/?username=Ismaelp96&repo=imdb-next&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&locale=pt-BR`}
+            alt='Ismaelp96 Git Stats language'
+            loading='lazy'
+            className='w-full h-auto'
+          />
         </ItemLayout>
       </div>
     </section>
