0f6aea00c2b3cbe699db3205edf7ab3ab502c84e
ismaelp96 <ismael6991@live.com>
2024-03-19 12:14:22 -0300

    Add EmailJs #9

--- src/app/(sub-pages)/contact/page.jsx
+++ src/app/(sub-pages)/contact/page.jsx
@@ -4,31 +4,31 @@ import bg from '../../../../public/background/contact-background.png';
 import Form from '@/components/contact/Form';
 
 export default function Contact() {
-  return (
-    <>
-      <Image
-        priority
-        sizes='100vw'
-        src={bg}
-        alt='background-image'
-        className='-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50'
-      />
-      <article className='relative w-full flex flex-col items-center justify-center space-y-8'>
-        <div className='flex flex-col items-center justify-center space-y-6 w-3/4'>
-          <h1 className='text-accent font-semibold text-center text-4xl capitalize'>
-            Summon the wizard
-          </h1>
-          <p className='text-center font-light'>
-            Step into the circle of enchantment and weave your words into the
-            fabric of the cosmos. Wheter you seek to conjure collaborations,
-            unlock mysteries, or simply share tales of adventure, your messages
-            are treasured scrolls within this realm. Use the form below to send
-            your missives through the ethereal network, and await the whisper of
-            magic in response.
-          </p>
-        </div>
-        <Form />
-      </article>
-    </>
-  );
+	return (
+		<>
+			<Image
+				priority
+				sizes='100vw'
+				src={bg}
+				alt='background-image'
+				className='-z-50 w-full h-full object-cover object-center opacity-50 fixed top-0 left-0'
+			/>
+			<article className='relative w-full flex flex-col items-center justify-center space-y-8'>
+				<div className='flex flex-col items-center justify-center space-y-6 w-3/4'>
+					<h1 className='text-accent font-semibold text-center text-4xl capitalize'>
+						Chame o mago
+					</h1>
+					<p className='text-center font-light'>
+						Entre no círculo do encantamento e entrelace suas palavras na
+						estrutura do cosmos. Quer você procure evocar colaborações,
+						desvendar mistérios ou simplesmente compartilhar histórias de
+						aventura, suas mensagens são pergaminhos preciosos neste reino.
+						Utilize o formulário abaixo para enviar suas missivas através da
+						rede etérea e aguarde o sussurro da magia em resposta.
+					</p>
+				</div>
+				<Form />
+			</article>
+		</>
+	);
 }
--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,42 +1,124 @@
 'use client';
+
 import { useForm } from 'react-hook-form';
 import React from 'react';
+import emailjs from '@emailjs/browser';
+import { Toaster, toast } from 'sonner';
 
 export default function FormContact() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className='max-w-md w-full flex flex-col items-center justify-center space-y-4'
-    >
-      <input
-        type='text'
-        placeholder='Nome'
-        {...register('name', { required: true })}
-        className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-      />
-      <input
-        type='email'
-        placeholder='Digite seu Email'
-        {...register('email', { required: true })}
-        className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-      />
-      <textarea
-        {...register('message', { required: true, max: 256, min: 50 })}
-        className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-      />
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-      <input
-        className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize transition-shadow duration-300 ease-in-out'
-        type='submit'
-        value='Cast your Message!'
-      />
-    </form>
-  );
+	const sendEmail = (params) => {
+		const toastId = toast.loading(
+			'Enviando sua mensagem, espere um momento...',
+		);
+
+		emailjs
+			.send(
+				process.env.NEXT_PUBLIC_SERVICE_ID,
+				process.env.NEXT_PUBLIC_TEMPLATE_ID,
+				params,
+				{
+					publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+					limitRate: {
+						throttle: 5000,
+					},
+				},
+			)
+			.then(
+				() => {
+					toast.success('Recebi sua mensagem, retornarei em breve!', {
+						id: toastId,
+					});
+				},
+				(error) => {
+					toast.error(
+						'Ocorreu um erro ao enviar sua mensagem, tente novamente mais tarde',
+						{
+							id: toastId,
+						},
+					);
+				},
+			);
+	};
+
+	const onSubmit = (data) => {
+		const templateParams = {
+			to_name: 'Ismael',
+			from_name: data.name,
+			reply_to: data.email,
+			message: data.message,
+		};
+		sendEmail(templateParams);
+	};
+
+	return (
+		<>
+			<Toaster richColors={true} />
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='max-w-md w-full flex flex-col items-center justify-center space-y-4'>
+				<input
+					type='text'
+					placeholder='Nome'
+					{...register('name', {
+						required: 'Esse campo é obrigatório!',
+						minLength: {
+							value: 3,
+							message: 'Nome precisa ter no minimo 3 caracters',
+						},
+					})}
+					className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+				/>
+				{errors.name && (
+					<span className='ml-2 inline-block self-start text-accent'>
+						{errors.name.message}
+					</span>
+				)}
+				<input
+					type='email'
+					placeholder='Digite seu Email'
+					{...register('email', {
+						required: 'E-mail é obrigatório',
+					})}
+					className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+				/>
+				{errors.email && (
+					<span className='ml-2 inline-block self-start text-accent'>
+						{errors.email.message}
+					</span>
+				)}
+
+				<textarea
+					{...register('message', {
+						required: 'Por gentileza, escreva sua mensagem',
+						maxLength: {
+							value: 256,
+							message: 'Limite de caracters foi ultrapassado',
+						},
+						minLength: {
+							value: 50,
+							message: 'É necessário ter mais de 50 caracteres',
+						},
+					})}
+					className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+					placeholder='Digite sua mensagem'
+				/>
+				{errors.name && (
+					<span className='ml-2 inline-block self-start text-accent'>
+						{errors.message.message}
+					</span>
+				)}
+				<input
+					className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize transition-shadow duration-300 ease-in-out'
+					type='submit'
+					value='Transmita sua mensagem!'
+				/>
+			</form>
+		</>
+	);
 }
